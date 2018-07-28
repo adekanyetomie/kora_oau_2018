f69b0de979d24ae4df4afa16531b3d306bbf0257
Israel Arunah <arunahisraelg2010@gmail.com>
2018-07-28 17:53:46 +0100

    Added messg

--- models/Ussd.js
+++ models/Ussd.js
@@ -4,13 +4,27 @@ const POST = axios.post;
 
 const validatePhone = (phone) => {
     return POST(VALIDATE_PHONE, phone)
-        .then((response) => response)
+        .then((response) => {
+            let resp = response == undefined ? {
+                status: false,
+                message: 'Looks like you do not have an account with us. Kindly register online.'
+            } : response;
+
+            return resp;
+        })
         .catch((err) => console.log('1', err));
 };
 
 const validatePin = (phone, pin) => {
     return POST(VALIDATE_PIN, {phone, pin})
-        .then((response) => response)
+        .then((response) => {
+            let resp = response == undefined ? {
+                status: false,
+                message: 'Looks like you do not have an account with us. Kindly register online.'
+            } : response;
+
+            return resp;
+        })
         .catch((err) => console.log('2', err));
 };
 
@@ -19,17 +33,17 @@ const transferToOtherCustomer = (sender_id, sender, recipient, amount, pin) => {
         return {status: false, message: 'Incorrect pin'}
     }
 
-   if (validatePhone(recipient).status === true) {
-       POST(TRANSFER_TO_OTHER_CUSTOMER, {
-           sender_id,
-           amount,
-           receiver_phone: recipient,
-           sender_phone: sender
-       })
-   }
-   else {
-       return {status: false, message: 'The owner of this phone number is not a customer'};
-   }
+    if (validatePhone(recipient).status === true) {
+        POST(TRANSFER_TO_OTHER_CUSTOMER, {
+            sender_id,
+            amount,
+            receiver_phone: recipient,
+            sender_phone: sender
+        })
+    }
+    else {
+        return {status: false, message: 'The owner of this phone number is not a customer'};
+    }
 };
 
 const transferToMyBank = (sender_id, pin) => {
